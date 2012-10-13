74bbb4a83238493b2066f2215b4924338af78d99
Ryan Barrett <git@ryanb.org>
2012-10-13 00:29:31 -0700

    uncomment

--- depuppet.sh
+++ depuppet.sh
@@ -21,15 +21,15 @@ visudo -c
 # add this to the end of /etc/rc.local:
 # echo disable > /proc/acpi/ibm/bluetooth
 
-# # X settings that may have been reset
-# xhost +
-# xset b off
-# xset -dpms
-# xset s off
+# X settings that may have been reset
+xhost +
+xset b off
+xset -dpms
+xset s off
 
-# # Let www-data (apache) through the firewall
-# iptables -I ufw-after-output 4 --match owner --uid-owner www-data -j ACCEPT
-# iptables -I ufw-after-output 5 --match owner --uid-owner postfix -j ACCEPT
+# Let www-data (apache) through the firewall
+iptables -I ufw-after-output 4 --match owner --uid-owner www-data -j ACCEPT
+iptables -I ufw-after-output 5 --match owner --uid-owner postfix -j ACCEPT
 
-# sed -i 's/^-A ufw-after-output -m owner --uid-owner googleadmin -j ACCEPT$/&\n-A ufw-after-output -m owner --uid-owner www-data -j ACCEPT\n-A ufw-after-output -m owner --uid-owner postfix -j ACCEPT/' \
-#   /etc/ufw/after.rules
+sed -i 's/^-A ufw-after-output -m owner --uid-owner googleadmin -j ACCEPT$/&\n-A ufw-after-output -m owner --uid-owner www-data -j ACCEPT\n-A ufw-after-output -m owner --uid-owner postfix -j ACCEPT/' \
+  /etc/ufw/after.rules
