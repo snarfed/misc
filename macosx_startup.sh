783149ef048fa968bf2104e8a2779110ffd82e15
Ryan Barrett <git@ryanb.org>
2015-01-18 16:04:21 -0800

    update path for new coreutils version, /usr/bin

--- macosx_startup.sh
+++ macosx_startup.sh
@@ -2,5 +2,5 @@
 #
 # A .login for Mac OS X. Inspired by:
 # http://stackoverflow.com/questions/135688/setting-environment-variables-in-os-x/5444960#5444960
-launchctl setenv PATH $HOME/bin:/usr/local/Cellar/coreutils/8.21/libexec/gnubin:/usr/local/bin:`launchctl getenv PATH`
-sudo /Library/StartupItems/VirtualBox/VirtualBox restart
+launchctl setenv PATH $HOME/bin:/usr/local/Cellar/coreutils/8.23_1/libexec/gnubin:/usr/local/bin:/usr/bin:`launchctl getenv PATH`
+# sudo /Library/StartupItems/VirtualBox/VirtualBox restart
