8717b2129464245734cbad90ec8b19f28964ccc8
Ryan Barrett <git@ryanb.org>
2015-11-07 12:14:14 -0800

    not working :(

--- flickcurl_download_set.sh
+++ flickcurl_download_set.sh
@@ -2,11 +2,13 @@
 #
 # Downloads all of the pictures in a flickr set
 #
+# NOT WORKING as of 5/26/2015. ~/bin/galdown.pl doesn't work either. :(
+#
 # Usage: flickcurl_download_set.sh [SET ID...]
 
-ids=`flickcurl -q photosets.getPhotos "$@" |& \
+ids=`flickcurl -q photosets.getPhotos "$@" | \
        grep -E -o 'ID [0-9]+' | cut -c4-`
 for id in $ids; do
-  wget `flickcurl -q photos.getSizes $id |& \
+  wget `flickcurl -q photos.getSizes $id | \
           grep -E -o 'http://.+_o.jpg'`
 done
