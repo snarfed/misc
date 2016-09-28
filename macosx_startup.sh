0ae8ca001b72f847e9444679ff04932b13467cc3
Ryan Barrett <git@ryanb.org>
2016-09-28 13:44:41 -0700

    macosx_startup.sh: coreutils 8.25

--- macosx_startup.sh
+++ macosx_startup.sh
@@ -2,7 +2,7 @@
 #
 # A .login for Mac OS X. Inspired by:
 # http://stackoverflow.com/questions/135688/setting-environment-variables-in-os-x/5444960#5444960
-launchctl setenv PATH $HOME/bin:/usr/local/Cellar/coreutils/8.23_1/libexec/gnubin:/usr/local/bin:/usr/bin:`launchctl getenv PATH`
+launchctl setenv PATH $HOME/bin:/usr/local/Cellar/coreutils/8.25/libexec/gnubin:/usr/local/bin:/usr/bin:`launchctl getenv PATH`
 # sudo /Library/StartupItems/VirtualBox/VirtualBox restart
 sudo /usr/sbin/apachectl -k start
 sudo pmset -a sleep 0
