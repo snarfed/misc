dc4accf4727ffc5792619d009302c4d3c45c6653
Ryan Barrett <git@ryanb.org>
2019-07-20 15:59:15 -0700

    macosx_startup: ssh-agent, gpg-agent, TMPDIR

--- macosx_startup.sh
+++ macosx_startup.sh
@@ -6,7 +6,7 @@ launchctl setenv PATH $HOME/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/loc
 # sudo /Library/StartupItems/VirtualBox/VirtualBox restart
 
 # this now happens automatically
-# sudo /usr/sbin/apachectl -k start
+sudo /usr/sbin/apachectl -k start
 
 # tentatively trying without this. 11/10/2017
 # sudo pmset -a sleep 0
@@ -16,4 +16,13 @@ source ~/color/color/setenv.sh
 # https://github.com/color/color/wiki/Database-Runbook
 clr pg:start
 
-ssh-add
+ssh-add ~/.ssh/id_rsa ~/.ssh/google_compute_engine
+
+eval $(gpg-agent --daemon)
+
+# Mac OS sets TMPDIR to something opaque like e.g.
+# /var/folders/y_/7mgz0yqd3hlgg2drhqx4ggf80000gn/T/
+# i originally used /tmp instead...but gave that up to get emacsclient working,
+# since it uses OS X's original dir.
+# setenv TMPDIR /tmp/
+ln -sf $TMPDIR /tmp/tmp
