53bcbb9d039575ee3a7dfa14524a1675b5dcf786
Ryan Barrett <git@ryanb.org>
2021-03-10 16:49:47 -0800

    macosx_startup.sh: drop Color stuff

--- macosx_startup.sh
+++ macosx_startup.sh
@@ -3,18 +3,9 @@
 # A .login for Mac OS X. Inspired by:
 # http://stackoverflow.com/questions/135688/setting-environment-variables-in-os-x/5444960#5444960
 launchctl setenv PATH $HOME/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/bin:`launchctl getenv PATH`
-# sudo /Library/StartupItems/VirtualBox/VirtualBox restart
 
 # this now happens automatically
 sudo /usr/sbin/apachectl -k start
-
-# tentatively trying without this. 11/10/2017
-# sudo pmset -a sleep 0
-
-cd ~/color/color
-source ~/color/color/setenv.sh
-# https://github.com/color/color/wiki/Database-Runbook
-clr pg:start
 
 ssh-add ~/.ssh/id_rsa ~/.ssh/google_compute_engine
 
