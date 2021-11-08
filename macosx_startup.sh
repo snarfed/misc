75ff9f19bee6bbb5f1cb855e53c0a14d942b3f70
Ryan Barrett <git@ryanb.org>
2021-11-07 19:21:44 -0800

    change for macOS Monterey: switch to Homebrew MySQL, let macOS start gpg-daemon

--- macosx_startup.sh
+++ macosx_startup.sh
@@ -4,12 +4,14 @@
 # http://stackoverflow.com/questions/135688/setting-environment-variables-in-os-x/5444960#5444960
 launchctl setenv PATH $HOME/bin:/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/bin:`launchctl getenv PATH`
 
-# this now happens automatically
-sudo /usr/sbin/apachectl -k start
+# Homebrew starts these automatically
+# brew services start mysql
+# brew services start httpd
 
 ssh-add ~/.ssh/id_rsa ~/.ssh/google_compute_engine ~/.ssh/ec2-keypair.pem ~/.ssh/azure-keypair.pem
 
-eval $(gpg-agent --daemon)
+# macOS starts this automatically
+# eval $(gpg-agent --daemon)
 
 # Mac OS sets TMPDIR to something opaque like e.g.
 # /var/folders/y_/7mgz0yqd3hlgg2drhqx4ggf80000gn/T/
