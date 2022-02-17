bd15f518beb3efc6cd5061e94876a929eaa568c0
Ryan Barrett <git@ryanb.org>
2022-02-17 08:29:00 -0800

    disconnect_goldtouch_keyboard.sh, shell script that can be made into a Mac .app

---
+++ disconnect_goldtouch_keyboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Put into eg this dir to make an app that you can put in the dock:
+# ~/Applications/disconnect_goldtouch_keyboard.app/Contents/MacOS/disconnect_goldtouch_keyboard 
+/opt/homebrew/bin/blueutil --disconnect 08-b7-38-24-13-3b --wait-disconnect 08-b7-38-24-13-3b
